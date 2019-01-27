b26e7c31d05de2350b65db4f799db92460dacd62
Andrey Shamardak <andrey.shamardak@gmail.com>
2019-01-27 14:02:50 +0200

    first commit

---
+++ server/build/precache-manifest.17722012460d157a5bb2a83ea2a23034.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "cb4688fa51d00b36c6f2",
+    "url": "./static/js/main.cb4688fa.chunk.js"
+  },
+  {
+    "revision": "5c6b6f3f2f78d42cd9b0",
+    "url": "./static/js/1.5c6b6f3f.chunk.js"
+  },
+  {
+    "revision": "cb4688fa51d00b36c6f2",
+    "url": "./static/css/main.798bc6b6.chunk.css"
+  },
+  {
+    "revision": "b153765a1187c9a26fb53772d112445a",
+    "url": "./index.html"
+  }
+];
---
+++ server/build/precache-manifest.bf9cdd38b6084b37c2cc294270ae09cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5538d145c6b63b697f3f",
+    "url": "./static/js/main.5538d145.chunk.js"
+  },
+  {
+    "revision": "5c6b6f3f2f78d42cd9b0",
+    "url": "./static/js/1.5c6b6f3f.chunk.js"
+  },
+  {
+    "revision": "5538d145c6b63b697f3f",
+    "url": "./static/css/main.fd16ffaa.chunk.css"
+  },
+  {
+    "revision": "ab5fcd70c5ac77e09644ad9c7668bba9",
+    "url": "./index.html"
+  }
+];
